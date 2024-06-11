6f822029c14aa70008e5ec88b90a1cd576af10a4
a.drozdov <a.drozdov@tinkoff.ru>
2024-06-11 18:25:48 +0300

    новое обновление

---
+++ telegram/common/texts_for_db.py
@@ -0,0 +1,28 @@
+from aiogram.utils.formatting import Bold, as_list, as_marked_section
+
+
+categories = ['100-300', '301-500', "501-1000"]
+
+description_for_info_pages = {
+    "main": "Добро пожаловать!",
+    "about": "Игрушки на 3Д принтере",
+    "payment": as_marked_section(
+        Bold("Варианты оплаты:"),
+        'перевод по ссылке На тинькофф банк https://www.tinkoff.ru/rm/drozdov.anatoliy4/PeuPP96769',
+        'наличкой',
+        marker="✅ ",
+    ).as_html(),
+    "shipping": as_list(
+        as_marked_section(
+            Bold("Варианты доставки/заказа:"),
+            "Самовынос (заберу на Владимирской площади)",
+            "Самовынос (заберу на работе в новом/старом офисе)",
+            marker="✅ ",
+        ),
+        as_marked_section(Bold("Нельзя:"), "Почта", "Голуби", marker="❌ "),
+        sep="\n----------------------\n",
+    ).as_html(),
+    'catalog': 'Категории:',
+    'cart': 'В корзине ничего нет!',
+    "order": 'Заказ'
+}
